da1a386ccdb23b4a479ab4b4f81b6ac0fce67cee
Dhruv Rajhans <149691783+CoderDhruv23@users.noreply.github.com>
2024-12-03 22:22:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+//import About from "./components/About";
+import Hero from "./components/hero";
+// import NavBar from "./components/Navbar";
+// import Features from "./components/Features";
+// import Story from "./components/Story";
+// import Contact from "./components/Contact";
+//import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      
+      <Hero />
+      
+      
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const button = ({title, id, rightIcon}) => {
+  return (
+    <div>{title}</div>
+  )
+}
+
+export default button
